9a74e1a045aae26e79d1f341524ff66e57944da1
ayakagi <37068236+ayakagi@users.noreply.github.com>
2018-04-23 15:51:25 +0100

    Add files via upload

---
+++ Scripts/DiceCheckZoneScript2.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceCheckZoneScript2 : MonoBehaviour
+{
+    Vector3 diceVelocity;
+    public Sprite[] DiceImage2;
+    public bool startRolling2;
+    public bool diceOnBoard2;
+
+    private void Start()
+    {
+        startRolling2 = false;
+        diceOnBoard2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        diceVelocity = DiceScript2.diceVelocity;
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        diceOnBoard2 = false;
+        if (startRolling2 == true)
+        {
+            if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f)
+            {
+                switch (col.gameObject.name)
+                {
+                    case "Side2.1":
+                        DiceNumberTextScript.diceNumber2 = 6;
+                        GameObject.Find("DiceIcon2").GetComponent<Image>().sprite = DiceImage2[5];
+                        break;
+                    case "Side2.2":
+                        DiceNumberTextScript.diceNumber2 = 5;
+                        GameObject.Find("DiceIcon2").GetComponent<Image>().sprite = DiceImage2[4];
+                        break;
+                    case "Side2.3":
+                        DiceNumberTextScript.diceNumber2 = 4;
+                        GameObject.Find("DiceIcon2").GetComponent<Image>().sprite = DiceImage2[3];
+                        break;
+                    case "Side2.4":
+                        DiceNumberTextScript.diceNumber2 = 3;
+                        GameObject.Find("DiceIcon2").GetComponent<Image>().sprite = DiceImage2[2];
+                        break;
+                    case "Side2.5":
+                        DiceNumberTextScript.diceNumber2 = 2;
+                        GameObject.Find("DiceIcon2").GetComponent<Image>().sprite = DiceImage2[1];
+                        break;
+                    case "Side2.6":
+                        DiceNumberTextScript.diceNumber2 = 1;
+                        GameObject.Find("DiceIcon2").GetComponent<Image>().sprite = DiceImage2[0];
+                        break;
+                }
+                diceOnBoard2 = true;
+            }
+        }
+    }
+}
