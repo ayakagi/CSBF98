ea34ecb02b772973ea15e6afd54daa91c8eeb388
le0u0 <37068236+ayakagi@users.noreply.github.com>
2018-04-23 17:36:30 +0100

    Add files via upload
    New Added Class - Property [17:36, 23/04/2018]

---
+++ Scripts/Property.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : MonoBehaviour {
+    private static int pptID;   // property ID, increment+1 from 1
+
+    private string pptName;     // property name
+    private int pptRent;        // rent value of property
+    private int houseCost;      // cost of each house of property
+    private int mongageValue;   // mongage value of property
+    private int[] houseNoRent;  // rent with 1/2/3/4 house(s)
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
